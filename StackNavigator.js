599df442717c0cf4748011c50b5e60ede563790e
Nishant-ruhil <nishant9053ruhil@gmail.com>
2024-05-12 15:36:15 +0530

    Home screen resuem will be continue

--- StackNavigator.js
+++ StackNavigator.js
@@ -2,13 +2,9 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import LoginScreen from './ExtraS/LoginScreen'
-import RegisterScreen from './ExtraS/RegisterScreen'
 import FirstScreen from './screens/FirstScreen'
 import HomeScreen from './screens/HomeScreen'
 import GSFirstScreen from './screens/GSFirstScreen'
-import GSSecondScreen from './ExtraS/GSSecondScreen'
-import GSThirdScreen from './ExtraS/GSThirdScreen'
 
 const StackNavigator = () => {
     const Stack = createNativeStackNavigator();
@@ -16,12 +12,8 @@ const StackNavigator = () => {
     <NavigationContainer>
         <Stack.Navigator>
             <Stack.Screen name="GSFirstscreen" component={GSFirstScreen} options={{headerShown: false}}/>
-            <Stack.Screen name="GSSecondscreen" component={GSSecondScreen} options={{headerShown: false}}/>
-            <Stack.Screen name="GSThirdScreen" component={GSThirdScreen} options={{headerShown: false}}/>
             <Stack.Screen name="FirstScreen" component={FirstScreen} options={{headerShown: false}}/>
-            <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
             <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
-            <Stack.Screen name="Register" component={RegisterScreen} options={{headerShown: false}}/>
         </Stack.Navigator>
     </NavigationContainer>
   )
