ea284cebb427eb2fdd90adecfa915d656ce3760a
Nishant-ruhil <nishant9053ruhil@gmail.com>
2024-05-14 22:39:37 +0530

    Update like in the post screen

--- api/index.js
+++ api/index.js
@@ -141,4 +141,49 @@ app.get("/all", async (req, res) => {
   }
 });
 
+//endpoints to like a post
+app.post("/like/:postId/:userId", async (req, res) => {
+  try {
+    const postId = req.params.postId;
+    const userId = req.params.userId;
 
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(400).json({ message: "Post not found" });
+    }
+
+    //check if the user has already liked the post
+    const existingLike = post?.likes.find(
+      (like) => like.user.toString() === userId
+    );
+
+    if (existingLike) {
+      post.likes = post.likes.filter((like) => like.user.toString() !== userId);
+    } else {
+      post.likes.push({ user: userId });
+    }
+
+    await post.save();
+
+    res.status(200).json({ message: "Post like/unlike successfull", post });
+  } catch (error) {
+    console.log("error likeing a post", error);
+    res.status(500).json({ message: "Error liking the post" });
+  }
+});
+
+
+//endpoint to update user description
+app.put("/profile/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const { userDescription } = req.body;
+
+    await User.findByIdAndUpdate(userId, { userDescription });
+
+    res.status(200).json({ message: "User profile updated successfully" });
+  } catch (error) {
+    console.log("Error updating user Profile", error);
+    res.status(500).json({ message: "Error updating user profile" });
+  }
+});
--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -1,14 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { RefreshControl, ScrollView, StyleSheet, Text, View, Image, Pressable ,Dimensions,StatusBar} from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { jwtDecode } from 'jwt-decode';
-import { Ionicons, Entypo, Feather, FontAwesome } from "@expo/vector-icons";
-import { SimpleLineIcons } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
+import React, { useEffect, useState, useCallback } from "react";
+import {
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  Dimensions,
+  SafeAreaView,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { jwtDecode } from "jwt-decode";
+import {
+  SimpleLineIcons,
+  AntDesign,
+  FontAwesome,
+  Feather,
+} from "@expo/vector-icons";
 import moment from "moment";
-import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
 
 const screenDimensions = Dimensions.get("screen");
 
@@ -16,8 +28,8 @@ const NewsScreen = () => {
   const [userId, setUserId] = useState("");
   const [user, setUser] = useState("");
   const [posts, setPosts] = useState([]);
-  const navigation = useNavigation();
   const [refreshing, setRefreshing] = useState(false);
+  const navigation = useNavigation();
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -41,14 +53,11 @@ const NewsScreen = () => {
   useEffect(() => {
     if (userId) {
       fetchUserProfile();
+      fetchAllPosts();
     }
   }, [userId]);
 
-  useEffect(() => {
-    fetchAllPosts();
-  }, []); // Fetch posts only once on component mount
-
-  const fetchUserProfile = async () => {
+  const fetchUserProfile = useCallback(async () => {
     try {
       const response = await axios.get(
         `http://192.168.0.111:4000/profile/${userId}`
@@ -58,41 +67,57 @@ const NewsScreen = () => {
     } catch (error) {
       console.log("Error fetching user profile", error);
     }
-  };
+  }, [userId]);
 
-  const fetchAllPosts = async () => {
+  const fetchAllPosts = useCallback(async () => {
     try {
       const response = await axios.get("http://192.168.0.111:4000/all");
-      setPosts(response.data.posts.reverse());
+      const reversedPosts = response.data.posts.reverse();
+      const postsWithLikes = reversedPosts.map((post) => ({
+        ...post,
+        isLiked: post.likes.some((like) => like.user === userId),
+      }));
+      setPosts(postsWithLikes);
     } catch (error) {
       console.log("Error Fetching Posts", error);
     }
-  };
+  }, [userId]);
 
   const MAX_LINES = 2;
   const [showfullText, setShowfullText] = useState(false);
 
-  const toggleShowFullText = () => {
-    setShowfullText(!showfullText);
-  };
+  const toggleShowFullText = useCallback(() => {
+    setShowfullText((prevState) => !prevState);
+  }, []);
 
-  const [isLiked, setIsLiked] = useState(false);
-
-  const handleLikePost = async (postId) => {
-    try {
-      const response = await axios.post(
-        `http://localhost:3000/like/${postId}/${userId}`
-      );
-      if (response.status === 200) {
-        const updatedPost = response.data.post;
-        setIsLiked(updatedPost.likes.some((like) => like.user === userId));
+  const handleLikePost = useCallback(
+    async (postId) => {
+      try {
+        const response = await axios.post(
+          `http://192.168.0.111:4000/like/${postId}/${userId}`
+        );
+        if (response.status === 200) {
+          const updatedPost = response.data.post;
+          const updatedPosts = posts.map((post) => {
+            if (post._id === postId) {
+              return {
+                ...post,
+                isLiked: updatedPost.likes.some((like) => like.user === userId),
+                likesCount: updatedPost.likes.length,
+              };
+            }
+            return post;
+          });
+          setPosts(updatedPosts);
+        }
+      } catch (error) {
+        console.log("Error liking/unliking the post", error);
       }
-    } catch (error) {
-      console.log("Error liking/unliking the post", error);
-    }
-  };
+    },
+    [userId, posts]
+  );
 
-  const onRefresh = async () => {
+  const onRefresh = useCallback(async () => {
     setRefreshing(true);
     try {
       await fetchAllPosts();
@@ -100,119 +125,117 @@ const NewsScreen = () => {
       console.log("Error Fetching Posts", error);
     }
     setRefreshing(false);
-  };
+  }, [fetchAllPosts]);
 
   return (
     <View style={styles.fullbody}>
       <SafeAreaView style={styles.container}>
-    <ScrollView
-      style={styles.scrollView}
-      refreshControl={
-        <RefreshControl
-          refreshing={refreshing}
-          onRefresh={onRefresh}
-          colors={["#0072b1"]}
-          tintColor={"#0072b1"}
-        />
-      }
-    >
-      <View>
-        {posts?.map((item, index) => (
-          <View key={index} style={styles.postContainer}>
-            <View style={styles.userInfo}>
-              <Image
-                style={styles.userAvatar}
-                source={{ uri: item?.user?.profileImage }}
-              />
-              <View style={styles.userInfoText}>
-                <Text style={styles.userName}>{item?.user?.name}</Text>
-                <Text style={styles.userDescription}>
-                  Engineer Graduate | LinkedIn Member
-                </Text>
-                <Text style={styles.postDate}>
-                  {moment(item.createdAt).format("MMMM Do YYYY")}
-                </Text>
-              </View>
-            </View>
-            <View style={styles.postContent}>
-              <Text
-                style={[
-                  styles.postDescription,
-                  showfullText && { maxHeight: "100%" },
-                ]}
-                numberOfLines={showfullText ? undefined : MAX_LINES}
-              >
-                {item?.description}
-              </Text>
-              {!showfullText && (
-                <Pressable onPress={toggleShowFullText}>
-                  <Text style={styles.readMore}>See more</Text>
-                </Pressable>
-              )}
-            </View>
-            <Image
-              style={styles.postImage}
-              source={{ uri: item?.imageUrl }}
+        <ScrollView
+          style={styles.scrollView}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              colors={["#0072b1"]}
+              tintColor={"#0072b1"}
             />
-            {item?.likes?.length > 0 && (
-              <View style={styles.likesContainer}>
-                <SimpleLineIcons name="like" size={16} color="#0072b1" />
-                <Text style={styles.likesCount}>{item?.likes?.length}</Text>
+          }
+        >
+          <View>
+            {posts.map((item, index) => (
+              <View key={index} style={styles.postContainer}>
+                <View style={styles.userInfo}>
+                  <Image
+                    style={styles.userAvatar}
+                    source={{ uri: item?.user?.profileImage }}
+                  />
+                  <View style={styles.userInfoText}>
+                    <Text style={styles.userName}>{item?.user?.name}</Text>
+                    <Text style={styles.userDescription}>
+                      Engineer Graduate | LinkedIn Member
+                    </Text>
+                    <Text style={styles.postDate}>
+                      {moment(item.createdAt).format("MMMM Do YYYY")}
+                    </Text>
+                  </View>
+                </View>
+                <View style={styles.postContent}>
+                  <Text
+                    style={[
+                      styles.postDescription,
+                      showfullText && { maxHeight: "100%" },
+                    ]}
+                    numberOfLines={showfullText ? undefined : MAX_LINES}
+                  >
+                    {item?.description}
+                  </Text>
+                  {!showfullText && (
+                    <Pressable onPress={toggleShowFullText}>
+                      <Text style={styles.readMore}>See more</Text>
+                    </Pressable>
+                  )}
+                </View>
+                <Image
+                  style={styles.postImage}
+                  source={{ uri: item?.imageUrl }}
+                />
+                {posts.length > 0 && item?.likesCount > 0 && (
+                  <View style={styles.likesContainer}>
+                    <SimpleLineIcons name="like" size={16} color="#0072b1" />
+                    <Text style={styles.likesCount}>{item?.likesCount}</Text>
+                  </View>
+                )}
+
+                <View style={styles.actionsContainer}>
+                  <Pressable onPress={() => handleLikePost(item?._id)}>
+                    <AntDesign
+                      name="like2"
+                      size={28}
+                      color={item?.isLiked ? "#0072b1" : "gray"}
+                    />
+                    <Text
+                      style={[
+                        styles.actionText,
+                        { color: item?.isLiked ? "white" : "gray" },
+                      ]}
+                    >
+                      Like
+                    </Text>
+                  </Pressable>
+                  <Pressable style={{ alignItems: "center" }}>
+                    <FontAwesome name="comment-o" size={28} color="gray" />
+                    <Text style={styles.actionText}>Comment</Text>
+                  </Pressable>
+                  <Pressable>
+                    <Feather name="send" size={28} color="gray" />
+                    <Text style={styles.actionText}>Send</Text>
+                  </Pressable>
+                </View>
+                <View
+                  style={{ height: 1, backgroundColor: "gray", marginTop: 10 }}
+                />
               </View>
-            )}
-            <View style={styles.actionsContainer}>
-              <Pressable onPress={() => handleLikePost(item?._id)}>
-                <AntDesign
-                  name="like2"
-                  size={24}
-                  color={isLiked ? "#0072b1" : "gray"}
-                />
-                <Text
-                  style={[
-                    styles.actionText,
-                    { color: isLiked ? "#0072b1" : "gray" },
-                  ]}
-                >
-                  Like
-                </Text>
-              </Pressable>
-              <Pressable>
-                <FontAwesome
-                  name="comment-o"
-                  size={20}
-                  color="gray"
-                />
-                <Text style={styles.actionText}>Comment</Text>
-              </Pressable>
-              <Pressable>
-                <Feather name="send" size={20} color="gray" />
-                <Text style={styles.actionText}>Send</Text>
-              </Pressable>
-            </View>
-            <View style={{height: 1,backgroundColor:'gray',marginTop:10}}/>
-              
+            ))}
           </View>
-          
-        ))}
-      </View>
-    </ScrollView>
+        </ScrollView>
       </SafeAreaView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    fullbody: {
-      flex: 1,
-      backgroundColor: "black",
-    },
-    container: {
-      height: screenDimensions.height - 180,
-      backgroundColor: "black",
-    },
-    scrollView: {
-      marginHorizontal: 18,
-    },
+  fullbody: {
+    flex: 1,
+    backgroundColor: "black",
+    paddingTop: 10,
+  },
+  container: {
+    height: screenDimensions.height - 180,
+    backgroundColor: "black",
+  },
+  scrollView: {
+    marginHorizontal: 18,
+  },
   postContainer: {
     marginBottom: 20,
   },
@@ -260,7 +283,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 240,
     marginBottom: 10,
-    borderRadius:10
+    borderRadius: 10,
   },
   likesContainer: {
     flexDirection: "row",
